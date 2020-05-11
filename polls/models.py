b4b92bad5e310f347bc9cd0370d9a5cc536ece9b
daniyalmanzoor28 <daniyalmanzoor28@gmail.com>
2020-05-11 09:26:23 +0500

    add __str__ in Choice and Question models

--- polls/models.py
+++ polls/models.py
@@ -14,5 +14,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     question = models.ForeignKey('Question', on_delete=models.CASCADE)
 
-    def __str__():
+    def __str__(self):
         return self.choice_text
