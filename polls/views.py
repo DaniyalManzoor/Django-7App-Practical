38aa6ef292d118fbc41aad034b4931cd72a20e8e
daniyalmanzoor28 <daniyalmanzoor28@gmail.com>
2020-05-12 10:58:55 +0500

    Update polls index.html

--- polls/views.py
+++ polls/views.py
@@ -6,9 +6,7 @@ from django.http import HttpResponse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
     return HttpResponse("You are looking at questions %s" % question_id)
@@ -21,3 +19,4 @@ def result(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're votes on question on %s " % question_id)
+
