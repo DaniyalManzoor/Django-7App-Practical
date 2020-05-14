45eae542da2d55be690757c42def77ada9b81f32
daniyalmanzoor28 <daniyalmanzoor28@gmail.com>
2020-05-14 17:50:45 +0500

    Update polls urls and view with lossely couple pattern

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 from django.http import HttpResponse, Http404
 # Create your views here.
@@ -8,12 +8,10 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question Does Not Exist")
-    return render(request, 'polls/detail.html', {'question' : question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def result(request, question_id):
@@ -23,4 +21,3 @@ def result(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're votes on question on %s " % question_id)
-
